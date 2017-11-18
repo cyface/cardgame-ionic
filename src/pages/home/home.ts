c2336c5ec7c5b80940d82b3823d244b78e3cbd9e
Tim White <tim@cyface.com>
2017-11-17 17:52:14 -0700

    stylin'!

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,13 +2,15 @@ import {Component} from '@angular/core';
 import {NavController} from 'ionic-angular';
 import {CreatePage} from "../create/create";
 import {JoinPage} from "../join/join";
+import {ToastController} from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private toastCtrl: ToastController) {
+    //this.presentToast();
   }
 
   goToCreate() {
@@ -17,6 +19,20 @@ export class HomePage {
 
   goToJoin() {
     this.navCtrl.push(JoinPage);
+  }
+
+  presentToast() {
+    let toast = this.toastCtrl.create({
+      message: 'HELLO DOLLY',
+      duration: 3000,
+      position: 'center'
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
