9879d57bc652ce3be85ca5d760b4d282f8cd2756
cyface <tim@cyface.com>
2018-01-10 10:20:17 -0700

    Adding environment files.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: true,
+  apiUrl: 'wss://service.cardgame.cyface.com/game/',
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,4 @@
+export const environment = {
+  production: false,
+  apiUrl: 'wss://service.cardgame.cyface.com/game/',
+};
